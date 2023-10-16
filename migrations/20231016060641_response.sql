0b741cd980d5885ebecf268e9f228d58248c5303
Turner Houghton <t.q.houghton@gmail.com>
2023-10-16 00:47:18 -0600

    add status_code column to response

--- migrations/20231016060641_response.sql
+++ migrations/20231016060641_response.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS response
 (
-    id          TEXT PRIMARY KEY NOT NULL,
-    name        TEXT,
-    headers     TEXT,
-    body        TEXT
+    id            TEXT PRIMARY KEY NOT NULL,
+    status_code   INT NOT NULL,
+    name          TEXT,
+    headers       TEXT,
+    body          TEXT
 );
