c46bf7d38413eeabee739e614ad44ed7793655a3
Michael Blydenburgh <mike.blydenburgh@gmail.com>
2023-12-27 09:27:02 -0500

    revert migration change

--- migrations/20231016060641_response.sql
+++ migrations/20231016060641_response.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS response
     id            TEXT PRIMARY KEY NOT NULL,
     status_code   INT NOT NULL,
     name          TEXT,
-    headers       JSON NOT NULL,
-    body          JSON
+    headers       TEXT,
+    body          TEXT
 );
